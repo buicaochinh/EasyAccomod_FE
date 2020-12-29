6b29b7a397e042ab803c72f8d189ad74f0a72b45
SpringBbe <tridung240400@gmail.com>
2020-12-29 23:38:17 +0700

    fix css post

--- src/views/room/index.vue
+++ src/views/room/index.vue
@@ -16,57 +16,86 @@
             </div>
             <div class="content block">
               <div class="author">
-                Người đăng: <a href=""> {{ author }} </a>
+                <strong>Người đăng: </strong><a href=""> {{ author }} </a>
               </div>
               <div class="columns">
-                <div class="column is-three-quarters">
-                  <span>Thể loại:</span>&nbsp;<span>{{ roomType }}</span>
-                  <br />
-                  <span>Địa chỉ:</span>&nbsp;<span>{{ detailAddress }}</span>
-                  <br />
-                  <span>Giá phòng:</span>&nbsp;<span>{{
-                    roomPrice | toCurrency
-                  }}</span
-                  >&nbsp;<span>(vnđ)</span> <br />
-                  <span>Tiền điện:</span>&nbsp;<span>{{
-                    electricityPrice | toCurrency
-                  }}</span
-                  >&nbsp;<span>(vnđ/kWh)</span> <br />
-                  <span>Tiền nước:</span>&nbsp;<span>{{
-                    waterPrice | toCurrency
-                  }}</span
-                  >&nbsp;<span>(vnđ/khối)</span> <br />
-                  <span>Thông tin chi tiết:</span>
-                  <p>{{ detail }}</p>
+                <div class="column">
+                  <div class="line">
+                    <span><strong>Thể loại:</strong></span
+                    >&nbsp;<span>{{ roomType }}</span>
+                    <br />
+                  </div>
+                  <div class="line">
+                    <span><strong>Địa chỉ:</strong></span
+                    >&nbsp;<span>{{ detailAddress }}</span>
+                    <br />
+                  </div>
+                  <div class="line">
+                    <span><strong>Giá phòng:</strong></span
+                    >&nbsp;<span>{{ roomPrice | toCurrency }}</span
+                    >&nbsp;<span></span> <br />
+                  </div>
+                  <div class="line">
+                    <span><strong>Tiền điện:</strong></span
+                    >&nbsp;<span>{{ electricityPrice | toCurrency }}</span
+                    >&nbsp;<span></span> <br />
+                  </div>
+                  <div class="line">
+                    <span><strong>Tiền nước:</strong></span
+                    >&nbsp;<span>{{ waterPrice | toCurrency }}</span
+                    >&nbsp;<span></span> <br />
+                  </div>
+                  <div class="line">
+                    <span><strong>Thông tin chi tiết:</strong></span>
+                    <p>{{ detail }}</p>
+                  </div>
+                  <div class="line">
+                    <span><strong>Tiện ích: </strong></span> <br />
+                    <table>
+                      <tr>
+                        <td>
+                          <span><strong>Chung chủ: </strong></span
+                          ><span>{{ withOwner === 1 ? "Có" : "Không" }}</span>
+                        </td>
+                        <td>
+                          <span><strong>Nhà vệ sinh: </strong></span
+                          ><span>{{
+                            restroom === 1 ? "Khép kín" : "Chung"
+                          }}</span>
+                        </td>
+                        <td>
+                          <span><strong>Nhà bếp: </strong></span
+                          ><span>{{ kitchen === 1 ? "Riêng" : "Chung" }}</span>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <span><strong>Điều hòa: </strong></span
+                          ><span>{{
+                            airConditioner === 1 ? "Có" : "Không"
+                          }}</span>
+                        </td>
+                        <td>
+                          <span><strong>Nóng lạnh: </strong></span
+                          ><span>{{ waterHeater === 1 ? "Có" : "Không" }}</span>
+                        </td>
+                        <td>
+                          <span><strong>Ban công: </strong></span
+                          ><span>{{ balcony === 1 ? "Có" : "Không" }}</span>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                  <div class="line">
+                    <span><strong>Một số tiện ích khác: </strong></span>
+                    <span>{{ additionalAmenities }}</span>
+                    <br />
+                  </div>
+                  <div class="line">
+                    <span><strong>Địa điểm lớn gần đó: </strong></span
+                    ><span>{{ nearPlace }}</span>
+                  </div>
                 </div>
-                <div class="amenities column">
-                  <span>Tiện ích: </span> <br />
-                  <span>{{
-                    withOwner === 1 ? "Chung chủ" : "Không chung chủ"
-                  }}</span>
-                  <p>
-                    <span>Nhà vệ sinh: </span
-                    ><span>{{ restroom === 1 ? "Khép kín" : "Chung" }}</span>
-                    <br />
-                    <span>Nhà bếp: </span
-                    ><span>{{ kitchen === 1 ? "Riêng" : "Chung" }}</span>
-                    <br />
-                    <span>Điều hòa: </span
-                    ><span>{{ airConditioner === 1 ? "Có" : "Không" }}</span>
-                    <br />
-                    <span>Nóng lạnh: </span
-                    ><span>{{ waterHeater === 1 ? "Có" : "Không" }}</span>
-                    <br />
-                    <span>Ban công: </span
-                    ><span>{{ balcony === 1 ? "Có" : "Không" }}</span>
-                  </p>
-                </div>
-              </div>
-              <div>
-                <span>Một số tiện ích khác: </span>
-                <span>{{ additionalAmenities }}</span>
-                <br />
-                <span>Gần với </span><span>{{ nearPlace }}</span>
               </div>
             </div>
             <div class="report">
@@ -240,5 +269,8 @@ a {
 
 .banner-slide {
   background: url("../../assets/slide.jpg") no-repeat 0 80px;
+}
+.line {
+  margin-top: 0.75rem;
 }
 </style>
